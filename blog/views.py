8b05d8ac4b53380dff09a6f9b666022c8472b7f5
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-11-10 11:32:45 +0100

    created homeview and postdetailview

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def home(request):
-    context = {}
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {}
+#     return render(request, 'blog/home.html', context)
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
